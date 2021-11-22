f8901de6b38734c76d1de3204ca490bc711936dc
Nova <nova@suprnova.dev>
2021-11-22 16:58:24 -0500

    Add more game fields, patch Assets
    Assets were broken in a previous API update, as height and width were removed.

--- SpeedrunComSharp/Common/ImageAsset.cs
+++ SpeedrunComSharp/Common/ImageAsset.cs
@@ -5,22 +5,18 @@ namespace SpeedrunComSharp
     public class ImageAsset
     {
         public Uri Uri { get; private set; }
-        public int Width { get; private set; }
-        public int Height { get; private set; }
 
         private ImageAsset() { }
 
         public static ImageAsset Parse(SpeedrunComClient client, dynamic imageElement)
         {
-            if (imageElement == null || imageElement.uri == null || imageElement.width == null || imageElement.height == null)
+            if (imageElement == null || imageElement.uri == null)
                 return null;
 
             var image = new ImageAsset();
 
             var uri = imageElement.uri as string;
             image.Uri = new Uri(uri);
-            image.Width = (int)imageElement.width;
-            image.Height = (int)imageElement.height;
 
             return image;
         }
